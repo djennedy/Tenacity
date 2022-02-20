bd9c26e7ade238c3a6bb4cbe8282d660ad8b4816
djennedy <darren_jennedy@sfu,ca>
2022-02-20 01:08:39 -0800

    Re-added slash commands

--- src/main/java/com/tenacity/stormhacks/Main.java
+++ src/main/java/com/tenacity/stormhacks/Main.java
@@ -2,6 +2,11 @@ package com.tenacity.stormhacks;
 
 import com.tenacity.stormhacks.slashCommands.*;
 import org.javacord.api.entity.server.Server;
+import org.javacord.api.interaction.SlashCommand;
+import org.javacord.api.interaction.SlashCommandOptionBuilder;
+import org.javacord.api.interaction.SlashCommandOptionType;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,46 +22,41 @@ public class Main {
                 }
         );
 
-        //Debugging Server
-        Server myServer= tenacityApi.api.getServerById("890685750981586944").get();
-
         //Adding slash commands
-        //TODO: Change all create for server to global before submission
-//        SlashCommand help = SlashCommand.with("help","Shows how to use the bot.").createGlobal(tenacityApi.api).join();
-//        SlashCommand ping = SlashCommand.with("ping", "A simple ping pong command!").createGlobal(tenacityApi.api).join();
-//        SlashCommand test = SlashCommand.with("test","testing server only slash command").createForServer(myServer).join();
-//        SlashCommand praise = SlashCommand.with("praise","Shows all of the saved accomplishments of a user."
-//                , new SlashCommandOptionBuilder()
-//                        .setType(SlashCommandOptionType.USER)
-//                        .setName("User")
-//                        .setDescription("The user that you would like to show their accomplishments to.")
-//                        .setRequired(false)
-//                )
-//                .createForServer(myServer)
-//                .join();
-//        SlashCommand addPraise = SlashCommand.with("addpraise","Adds an accomplishment of a user",
-//                new SlashCommandOptionBuilder()
-//                        .setType(SlashCommandOptionType.USER)
-//                        .setName("User")
-//                        .setDescription("The user that you would like to add an accomplishment to")
-//                        .setRequired(true),
-//                new SlashCommandOptionBuilder()
-//                        .setType(SlashCommandOptionType.STRING)
-//                        .setName("Accomplishment")
-//                        .setDescription("The accomplishment you would like to add")
-//                        .setRequired(true)
-//                )
-//                .createForServer(myServer)
-//                .join();
-
-//        SlashCommand removePraise = SlashCommand.with("rmpraise", "Removes one of your accomplishments",
-//                new SlashCommandOptionBuilder()
-//                        .setType(SlashCommandOptionType.DECIMAL)
-//                        .setName("Index")
-//                        .setDescription("Index number of the accomplishment you would like to remove. Defaults to most recent if not given.")
-//                        .setRequired(false))
-//                .createForServer(myServer)
-//                .join();
+        SlashCommand help = SlashCommand.with("help","Shows how to use the bot.").createGlobal(tenacityApi.api).join();
+        SlashCommand ping = SlashCommand.with("ping", "A simple ping pong command!").createGlobal(tenacityApi.api).join();
+        SlashCommand praise = SlashCommand.with("praise","Shows all of the saved accomplishments of a user."
+                , new SlashCommandOptionBuilder()
+                        .setType(SlashCommandOptionType.USER)
+                        .setName("User")
+                        .setDescription("The user that you would like to show their accomplishments to.")
+                        .setRequired(false)
+                )
+                .createGlobal(tenacityApi.api)
+                .join();
+        SlashCommand addPraise = SlashCommand.with("addpraise","Adds an accomplishment of a user",
+                new SlashCommandOptionBuilder()
+                        .setType(SlashCommandOptionType.USER)
+                        .setName("User")
+                        .setDescription("The user that you would like to add an accomplishment to")
+                        .setRequired(true),
+                new SlashCommandOptionBuilder()
+                        .setType(SlashCommandOptionType.STRING)
+                        .setName("Accomplishment")
+                        .setDescription("The accomplishment you would like to add")
+                        .setRequired(true)
+                )
+                .createGlobal(tenacityApi.api)
+                .join();
+        SlashCommand removePraise = SlashCommand.with("rmpraise", "Removes one of your accomplishments",
+                new SlashCommandOptionBuilder()
+                        .setType(SlashCommandOptionType.DECIMAL)
+                        .setName("Index")
+                        .setDescription("Index number of the accomplishment you would like to remove. Defaults to most recent if not given.")
+                        .setRequired(false)
+                )
+                .createGlobal(tenacityApi.api)
+                .join();
 
         //Listeners
         HelpListener helpListener = new HelpListener();
@@ -64,8 +64,6 @@ public class Main {
         AddPraiseListener addPraiseListener = new AddPraiseListener();
         RemovePraiseListener removePraiseListener = new RemovePraiseListener();
         PraiseListener showPraiseListener = new PraiseListener();
-
-
 
     }
 }
